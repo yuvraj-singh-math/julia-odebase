223c01adcb58c90fc9c91fd62545899ba728bf83
yuvraj-singh-math <fdmm79@durham.ac.uk>
2024-10-12 19:46:43 +0100

    Rename, fix bugs with some systems

--- src/odes/BIOMD0000000346.jl
+++ src/odes/BIOMD0000000346.jl
@@ -2,7 +2,7 @@
 using Oscar;
 paramsRing,(k1,k2,k3,k4,k5)=rational_function_field(QQ,["k1","k2","k3","k4","k5"])
 polRing,(x1,x2)=polynomial_ring(paramsRing,["x1","x2"])
-chemSystem=[k3*(x1 + (-x1^3//3) + x2 + k4), (-1/k3)*(x1 + (-k1) + k2*x2) ]
+chemSystem=[k3*(x1 + (-x1^3/3) + x2 + k4), (-1/k3)*(x1 + (-k1) + k2*x2) ]
 constraints=[ ]
 ID="BIOMD0000000346"
 pol=true
--- src/odes/BIOMD0000000552.jl
+++ src/odes/BIOMD0000000552.jl
@@ -3,7 +3,7 @@ using Oscar;
 paramsRing,(k1,k2,k3,k4,k5)=rational_function_field(QQ,["k1","k2","k3","k4","k5"])
 polRing,(x1,x2,x3)=polynomial_ring(paramsRing,["x1","x2","x3"])
 chemSystem=[(-1)*k5*k1*x1*x2/k5, (1*k5*k2 + (-1)*k5*k3*x1 + (-1)*k5*k4*x2)/k5 ]
-constraints=[ x3 - x1//50 ]
+constraints=[ x3 - x1/50 ]
 ID="BIOMD0000000552"
 pol=true
 mass_bool=false
